4143fce48c6584cbe40856f571df51ae03876200
Albert Ferran Casas <aferranc@cirsa.com>
2024-09-07 11:35:40 +0200

    feat(custom.jss): Configured info layout and translated some phrases

--- refactored_spoon/static/js/custom.js
+++ refactored_spoon/static/js/custom.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
     var table = $('#restaurantTable').DataTable({
-        order: [[0, 'asc'], [2, 'asc']]
+        language: {
+            search: 'Cerca:',
+            entries: {
+                _: 'entrades',
+                1: 'entrada'
+            }
+        },
+        order: [[0, 'asc'], [2, 'asc']],
+        layout: {
+            topStart: {
+                pageLength: {
+                    menu: [5, 10, 25, 50]
+                }
+            },
+            topEnd: {
+                search: {
+                    processing: true
+                }
+            },
+            bottomStart: {
+                info: {
+                    info: 'Showing page _PAGE_ of _PAGES_',
+                    text: 'Es mostren _START_ a _END_ _ENTRIES-TOTAL_',
+                    empty: 'No hi ha entrades per mostrar',
+                    search: ' (filtrat de _MAX_ entrades)'
+                }
+            },
+            bottomEnd: {
+                paging: {
+                    numbers: true
+                }
+            }
+        }
     });
 });
