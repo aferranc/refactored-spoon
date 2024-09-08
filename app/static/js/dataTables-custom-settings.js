35cb489bcf2eca21ac947f26c6660c495f403480
Albert Ferran Casas <aferranc@cirsa.com>
2024-09-08 15:54:47 +0200

    refactor: Remove language translations

--- app/static/js/dataTables-custom-settings.js
+++ app/static/js/dataTables-custom-settings.js
@@ -5,14 +5,6 @@ $(document).ready(function() {
         "columnDefs": [
             { "searchable": false, "targets": 6 }  // Disable search in the "Actions" column
         ],
-        // Configure the language settings for the DataTable
-        language: {
-            search: 'Cerca:',
-            entries: {
-                _: 'entrades',
-                1: 'entrada'
-            }
-        },
         // Set the initial order of the columns
         order: [[0, 'asc'], [2, 'asc']],
         // Configure the layout of the DataTable
@@ -28,12 +20,7 @@ $(document).ready(function() {
                 }
             },
             bottomStart: {
-                info: {
-                    info: 'Showing page _PAGE_ of _PAGES_',
-                    text: 'Es mostren _START_ a _END_ _ENTRIES-TOTAL_',
-                    empty: 'No hi ha entrades per mostrar',
-                    search: ' (filtrat de _MAX_ entrades)'
-                }
+                info: true  // Enable page info
             },
             bottomEnd: {
                 paging: {
