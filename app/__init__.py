7267f08ea58ecbeb460258154f887af2ffa1151f
Albert Ferran Casas <aferranc@cirsa.com>
2024-09-08 18:55:56 +0200

    fix: Move the configuration file to top directory

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from app.config import Config
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "jXthea5ednWrlExO1WJfewOq6COYPE3N"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "restaurants.db")
