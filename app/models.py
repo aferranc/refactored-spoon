9a550c1a2a11bf9239bade61eecf4ba2acc68aec
Albert Ferran Casas <aferranc@cirsa.com>
2024-09-08 12:53:05 +0200

    refactor: Add restaurant tables and relationships

--- app/models.py
+++ app/models.py
@@ -13,3 +13,66 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User {}>".format(self.username)
+
+
+class Country(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(80), index=True, unique=True)
+
+    regions: so.WriteOnlyMapped["Region"] = so.relationship(back_populates="countries")
+
+    def __repr__(self):
+        return "<Country {}>".format(self.name)
+
+
+class Region(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(80), index=True, unique=True)
+    country_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Country.id), index=True)
+
+    countries: so.Mapped[Country] = so.relationship(back_populates="regions")
+    provinces: so.WriteOnlyMapped["Province"] = so.relationship(back_populates="regions")
+
+    def __repr__(self):
+        return "<Region {}>".format(self.name)
+
+
+class Province(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(80), index=True, unique=True)
+    region_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Region.id), index=True)
+
+    regions: so.Mapped[Region] = so.relationship(back_populates="provinces")
+    cities: so.WriteOnlyMapped["City"] = so.relationship(back_populates="provinces")
+
+    def __repr__(self):
+        return "<Province {}>".format(self.name)
+
+
+class City(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(80), index=True, unique=True)
+    province_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Province.id), index=True)
+
+    provinces: so.Mapped[Province] = so.relationship(back_populates="cities")
+
+    def __repr__(self):
+        return "<City {}>".format(self.name)
+
+
+class Restaurant(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(80), index=True, unique=True)
+    address: so.Mapped[str] = so.mapped_column(sa.String(120), nullable=False)
+    city_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(City.id), index=True)
+    province_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Province.id), index=True)
+    region_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Region.id), index=True)
+    country_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Country.id), index=True)
+
+    cities: so.Mapped[City] = so.relationship()
+    provinces: so.Mapped[Province] = so.relationship()
+    regions: so.Mapped[Region] = so.relationship()
+    countries: so.Mapped[Country] = so.relationship()
+
+    def __repr__(self):
+        return "<Restaurant {}>".format(self.name)
