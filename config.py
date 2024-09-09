b23ea49e4b5f7b0f9bd4cf747ba60ab135ea1770
Albert Ferran Casas <aferranc@cirsa.com>
2024-09-09 09:42:00 +0200

    refactor: Set Catalan as default language

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "jXthea5ednWrlExO1WJfewOq6COYPE3N"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "restaurants.db")
-    LANGUAGES = ["en", "ca", "es"]
+    LANGUAGES = ["ca", "es"]
+    BABEL_DEFAULT_LOCALE = "ca"
