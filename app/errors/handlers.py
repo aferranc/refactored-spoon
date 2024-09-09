e435d0c5fc0c85c394b6fe744ae5a09c86ca3e8e
Albert Ferran Casas <aferranc@cirsa.com>
2024-09-09 12:42:11 +0200

    feat: Initialize blueprints

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,14 +1,16 @@
-from flask import render_template
+from flask import Blueprint, render_template
 
-from app import app, db
+from app import db
+
+bp = Blueprint("errors", __name__)
 
 
-@app.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template("404.html"), 404
 
 
-@app.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template("500.html"), 500
